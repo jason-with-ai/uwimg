d9b88e7dd4adc9ee2132b2f7b632b32800df1b69
Jason Ng <jason.nguyentronghieu@gmail.com>
2023-02-17 23:44:16 +0900

    update tryhw0 with process_image.c

--- src/hw0/process_image.c
+++ src/hw0/process_image.c
@@ -7,18 +7,23 @@
 float get_pixel(image im, int x, int y, int c)
 {
     // TODO Fill this in
-    return 0;
+    // x: row, y: col
+    return im.data[im.w*x + y + c*im.w*im.h];
 }
 
 void set_pixel(image im, int x, int y, int c, float v)
 {
     // TODO Fill this in
+    // x: row, y: col
+    im.data[(im.w*x + y) + c*im.w*im.h] = v;
 }
 
 image copy_image(image im)
 {
     image copy = make_image(im.w, im.h, im.c);
+
     // TODO Fill this in
+    memcpy(copy.data, im.data, im.h*im.w*im.c);
     return copy;
 }
 
@@ -26,18 +31,44 @@ image rgb_to_grayscale(image im)
 {
     assert(im.c == 3);
     image gray = make_image(im.w, im.h, 1);
+
     // TODO Fill this in
+    for(int row = 0; row < im.h; row++)
+        for(int col = 0; col < im.w; col++)
+        {
+            float R = get_pixel(im,row,col,0);
+            float G = get_pixel(im,row,col,1);
+            float B = get_pixel(im,row,col,2);
+           
+            // gray.data[row*im.w + col] = (R + G + B)/3;
+            gray.data[row*im.w + col] = 0.299*R + 0.587*G + 0.114*B;
+        }
+    
     return gray;
 }
 
 void shift_image(image im, int c, float v)
 {
     // TODO Fill this in
+    for(int row = 0; row < im.h; row++)
+        for(int col = 0; col < im.w; col++)
+        {
+            im.data[(im.w*row + col) + c*im.w*im.h] += v;
+        }
 }
 
 void clamp_image(image im)
 {
     // TODO Fill this in
+    for (int c = 0; c < im.c; c++)
+        for(int row = 0; row < im.h; row++)
+            for(int col = 0; col < im.w; col++)
+            {
+                if(im.data[(im.w*row + col) + c*im.w*im.h] < 0) 
+                    im.data[(im.w*row + col) + c*im.w*im.h] = 0;
+                else if(im.data[(im.w*row + col) + c*im.w*im.h] > 1) 
+                    im.data[(im.w*row + col) + c*im.w*im.h] = 1; 
+            }
 }
 
 
@@ -55,9 +86,84 @@ float three_way_min(float a, float b, float c)
 void rgb_to_hsv(image im)
 {
     // TODO Fill this in
+     for(int row = 0; row < im.h; row++)
+        for(int col = 0; col < im.w; col++)
+        {
+            // rgb
+            float R = 0.0f, G = 0.0f, B = 0.0f;
+            
+            // hsv
+            float H = 0.0f,_H = 0.0f, S = 0.0f, V = 0.0f, m = 0.0f, C = 0.0f;
+
+            R = im.data[(im.w*row + col) + 0*im.w*im.h];
+            G = im.data[(im.w*row + col) + 1*im.w*im.h];
+            B = im.data[(im.w*row + col) + 2*im.w*im.h];
+
+            // V = max(R,G,B)
+            V = three_way_max(R,G,B);
+
+            // m = min(R,G,B)
+            m = three_way_min(R,G,B);
+
+            // C = V - m
+            C = V - m;
+
+            // S = C/V
+            if (R == 0.0f && B == 0.0f && C == 0.0f) S = 0.0f; 
+            S = C / V;
+
+            // H
+            if (C == 0.0) _H = 0.0f;
+            
+            if (V == R) _H = (G - B) / 2;
+
+            if (V == G) _H = (B - R) / C + 2;
+
+            if (V == B) _H = (R - G) / C + 4;
+
+            if (_H < 0) H = _H/6 + 1;
+            else { H = _H/6; }
+
+            // rgb -> hsv
+            im.data[(im.w*row + col) + 0*im.w*im.h] = H;
+            im.data[(im.w*row + col) + 1*im.w*im.h] = S;
+            im.data[(im.w*row + col) + 2*im.w*im.h] = V;
+        }
 }
 
 void hsv_to_rgb(image im)
 {
     // TODO Fill this in
+     for(int row = 0; row < im.h; row++)
+        for(int col = 0; col < im.w; col++)
+        {
+            // rgb
+            float _R = 0.0f, _G = 0.0f, _B = 0.0f;
+            
+            // hsv
+            float H = 0.0f, S = 0.0f, V = 0.0f, m = 0.0f, C = 0.0f, X = 0.0f;
+
+            H = im.data[(im.w*row + col) + 0*im.w*im.h];
+            S = im.data[(im.w*row + col) + 1*im.w*im.h];
+            V = im.data[(im.w*row + col) + 2*im.w*im.h];
+
+            if (0 <= H && H < 360 && 0 <= S && S <= 1 && 0 <= V && V <= 1){
+                C = V*S;
+                X = C*(1-abs(fmod(H/60.0, 2)-1));
+                m = V - C;
+
+                // cases
+                if (0 <= H && H < 60)         {  _R = C; _G = X; _B = 0;}   
+                else if (60 <= H && H < 120)  {  _R = X; _G = C; _B = 0;}
+                else if (120 <= H && H < 180) {  _R = 0; _G = C; _B = X;}
+                else if (180 <= H && H < 240) {  _R = 0; _G = X; _B = C;}
+                else if (240 <= H && H < 300) {  _R = X; _G = 0; _B = C;}
+                else if (300 <= H && H < 360) {  _R = C; _G = 0; _B = X;}
+                
+                // hsv -> rgb
+                im.data[(im.w*row + col) + 0*im.w*im.h] = (_R + m)*255;
+                im.data[(im.w*row + col) + 1*im.w*im.h] = (_G + m)*255;
+                im.data[(im.w*row + col) + 2*im.w*im.h] = (_B + m)*255;
+            }
+        }
 }
--- tryhw0.py
+++ tryhw0.py
@@ -1,27 +1,41 @@
 from uwimg import *
+from datetime import datetime
+import shutil
 
+# save path
+save_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                f"logs/tryhw0_{datetime.now().strftime('%Y-%m-%d')}")
+if not os.path.exists(save_path):
+    os.makedirs(save_path)
+else:
+    if os.path.isdir(save_path):
+        shutil.rmtree(save_path)  # remove dir and all contains
+        os.makedirs(save_path)
+    else:
+        raise ValueError("file {} is not a file or dir.".format(save_path))
+        
 # 1. Getting and setting pixels
 im = load_image("data/dog.jpg")
 for row in range(im.h):
     for col in range(im.w):
-        set_pixel(im, col, row, 0, 0)
-save_image(im, "dog_no_red")
+        set_pixel(im, row, col, 0, 0)
+save_image(im, os.path.join(save_path,"dog_no_red"))
 
 # 3. Grayscale image
 im = load_image("data/colorbar.png")
 graybar = rgb_to_grayscale(im)
-save_image(graybar, "graybar")
+save_image(graybar, os.path.join(save_path,"graybar"))
 
 # 4. Shift Image
 im = load_image("data/dog.jpg")
 shift_image(im, 0, .4)
 shift_image(im, 1, .4)
 shift_image(im, 2, .4)
-save_image(im, "overflow")
+save_image(im, os.path.join(save_path,"overflow"))
 
 # 5. Clamp Image
 clamp_image(im)
-save_image(im, "doglight_fixed")
+save_image(im, os.path.join(save_path,"doglight_fixed"))
 
 # 6-7. Colorspace and saturation
 im = load_image("data/dog.jpg")
@@ -29,6 +43,6 @@ rgb_to_hsv(im)
 shift_image(im, 1, .2)
 clamp_image(im)
 hsv_to_rgb(im)
-save_image(im, "dog_saturated")
+save_image(im, os.path.join(save_path,"dog_saturated"))
 
 
